8ab525d4e11d1d238b1a99575040b758dddbe373
BIOCISHERE <bioc.is.here.8@gmail.com>
2022-12-30 02:23:45 +0000

    final

--- src/app.js
+++ src/app.js
@@ -5,7 +5,30 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
+window.onload = () => {
   //write your code here
+  document.querySelector("#btn").addEventListener("click", () => {
+    document.querySelector("#the-excuse").innerHTML = generateExcuse();
+  });
   console.log("Hello Rigo from the console!");
+};
+let generateExcuse = () => {
+  let who = [
+    "The Dog",
+    "A Jogger",
+    "The Racoon",
+    "The Driver",
+    "A Comedian",
+    "A Pincone"
+  ];
+  let action = ["Took my", "Threw my", "Yelled at my", "Stole my", "Bit my"];
+  let what = ["Homework", "Toe", "Car", "Shoe"];
+  let when = ["On the street", "In my house", "In my driveway"];
+
+  let whoIndx = Math.floor(Math.random() * who.length);
+  let actnIndx = Math.floor(Math.random() * action.length);
+  let whatIndx = Math.floor(Math.random() * what.length);
+  let whenIndx = Math.floor(Math.random() * when.length);
+
+  return `${who[whoIndx]} ${action[actnIndx]} ${what[whatIndx]} ${when[whenIndx]}`;
 };
